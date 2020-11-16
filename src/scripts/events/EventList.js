277b3e44a42eec246da8de2fd303ea628d31cdb4
Dillon Draper <dillonbdraper@gmail.com>
2020-11-16 15:20:25 -0600

    added sort by date and special class for soonest event

--- src/scripts/events/EventList.js
+++ src/scripts/events/EventList.js
@@ -1,11 +1,11 @@
-import { Event } from "./Event.js"
+import { Event, FirstEvent } from "./Event.js"
 import { deleteEvent, getEvents, useEvents } from "./EventDataProvider.js"
 import { EventForm } from "./EventForm.js"
 
 const eventHub = document.querySelector(".container")
 
 
-//Renders all events to the DOM
+//Renders all events to the DOM, ensuring that soonest event is displayed first and has a special class
 export const EventList = () => {
     const displayTarget = document.querySelector(".events__display")
 
@@ -16,11 +16,29 @@ export const EventList = () => {
         .then(() => {
             allEvents = useEvents()
 
+            // Filters all events down to only events for the current user
             const currentEvents = allEvents.filter(events => events.userId === userIdNumber)
+            
+            // Sorts events by date, oldest to newest
+            const eventsByDate = currentEvents.slice().sort((a, b) =>  {
+                return new Date(a.date) - new Date(b.date)
+            })
+     
+            // Returns the chronologically soonest future vent
+            const nearestEvent = eventsByDate.find(event => Date.now() < Date.parse(event.date))
+            
+            const indexToRemove = eventsByDate.indexOf(nearestEvent)
 
-            displayTarget.innerHTML = currentEvents.map(event => {
+            eventsByDate.splice(indexToRemove, 1)
+
+                    
+            // Creates 2 strings, one for the nearestEvent and one for all other events and adds them together and appends to the DOM
+            const firstEventString = FirstEvent(nearestEvent)
+            const otherEventsString = eventsByDate.map(event => {
                 return Event(event)
             }).join("")
+
+            displayTarget.innerHTML = firstEventString + otherEventsString
         })
 
 }
