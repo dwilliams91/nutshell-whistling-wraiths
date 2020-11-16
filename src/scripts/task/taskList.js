3ec633a51af6f97b4f753a1f77c3616cd40c7ed2
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-16 11:01:18 -0600

    Wrote functions to display tasks related to the current user to the DOM

--- src/scripts/task/taskList.js
+++ src/scripts/task/taskList.js
@@ -1,17 +1,32 @@
 // This module is responsible for rendering task information to the DOM
 import { getUsers, useUsers } from "../user/UserDataProvider.js"
+import { taskHtml } from "./task.js"
 import {getTasks, useTasks} from "./TaskProvider.js"
 
 // set these to empty arrays with let so that I can access them inside multiple functions if necessary
 let userArr = []
 let taskArr = []
 
+// Render the current user's tasks to the DOM
 export const taskList = () => {
     getUsers()
     .then(getTasks)
     .then(() => {
         userArr = useUsers()
         taskArr = useTasks()
-        console.log("users: ", userArr, "tasks: ", taskArr)
+        render()
     })
+}
+
+const render = () => {
+    // Select the html element to render to
+    const contentContainer = document.querySelector(".tasks__display")
+    // get the id of the current user, and convert it to a number instead of a string
+    const id = parseInt(sessionStorage.getItem("activeUser"))
+    // get the tasks related to the current user
+    const relatedTasks = taskArr.filter(task => task.userId === id)
+    // Iterate through the related tasks and generate the html
+    const taskString = relatedTasks.map(task => taskHtml(task))
+    // place the newly generated html in the element previously selected
+    contentContainer.innerHTML = taskString
 }
