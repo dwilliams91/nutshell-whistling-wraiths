440bd8ea09651b278b04b226009024d55c5ef969
Dillon Draper <dillonbdraper@gmail.com>
2020-11-19 09:15:04 -0600

    Commented and refactored code into easier to read functions

--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -81,11 +81,12 @@ eventHub.addEventListener("click", e => {
         formTarget.innerHTML = ""
     }
 
+    //performs similar function to above but for updating and transmits additional eventId detail
     if (e.target.id.startsWith("update__")) {
         const date = document.getElementById("eventDate").value
         const name = document.getElementById("eventName").value
         const location = document.getElementById("eventLocation").value
-       
+
         const eventId = e.target.id.split("__")[1]
 
         const updateEvent = new CustomEvent("updateEvent", {
@@ -103,20 +104,18 @@ eventHub.addEventListener("click", e => {
 
 })
 
-
+//Listens for save event, saves the event, and updates the DOM.
 eventHub.addEventListener("saveEvent", e => {
-            saveEvent(e.detail)
-            .then(EventList)
-            .then(() => {
+    saveEvent(e.detail)
+        .then(EventList)
+        .then(() => {
+            const formTarget = document.querySelector(".event__form")
+            formTarget.innerHTML = ""
+        })
 
-                
-
-                const formTarget = document.querySelector(".event__form")
-                formTarget.innerHTML = ""
-            })
-    
 })
 
+//Listens for update event, updates it, and updates the DOM.
 eventHub.addEventListener("updateEvent", e => {
     editEvent(e.detail.eventId, e.detail)
         .then(EventList)
@@ -127,7 +126,8 @@ eventHub.addEventListener("updateEvent", e => {
         })
 })
 
-eventHub.addEventListener("click", e=> {
+//Creats weather custom event and dispatches it to the DOM with necessary details
+eventHub.addEventListener("click", e => {
     if (e.target.id.startsWith("showWeather")) {
         console.log(e.target.id)
         const eventTargetId = parseInt(e.target.id.split("__")[1])
@@ -145,18 +145,27 @@ eventHub.addEventListener("click", e=> {
     }
 })
 
+//Uses info dispatched updateWeather event to call weatherMaker with all four parameters
 eventHub.addEventListener("updateWeather", e => {
+    //Gets City, ST formatted location and date from weather event
     const locationArray = e.detail.location.split(", ")
     const date = e.detail.date
-    getGeocode(locationArray[0], locationArray[1])
-    .then( () => {
-        const coordinates = useGeocode()[0]
 
-        console.log(coordinates)
-        const cityName = coordinates.city
-        console.log(cityName)
-        const lat = coordinates.point.lat
-        const long = coordinates.point.lng
-        weatherMaker(lat, long, cityName, date)
-    })
+    //Converts that location into geocode
+    getGeocode(locationArray[0], locationArray[1])
+        .then(() => {
+            const coordinates = useGeocode()[0]
+            console.log(coordinates)
+
+            //Uses geocode object to get city name
+            let cityName = ""
+            if (coordinates.city) {
+                cityName = coordinates.city
+            } else {
+                cityName = coordinates.name
+            }
+            const lat = coordinates.point.lat
+            const long = coordinates.point.lng
+            weatherMaker(lat, long, cityName, date)
+        })
 })
